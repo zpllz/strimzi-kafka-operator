d60aeef67460ebe9111a8962220bd3c235ca13c4
zhangpeng <pengzhang@yunify.com>
2024-03-19 17:15:17 +0800

    Feature:use distroless image

--- docker-images/kafka-based/kafka/stunnel-scripts/entity_operator_stunnel_pre_stop.sh
+++ docker-images/kafka-based/kafka/stunnel-scripts/entity_operator_stunnel_pre_stop.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-while true; do
-  CONNS=$(netstat -ant | grep -w 127.0.0.1:2181 | grep -c ESTABLISHED)
+sleep 60
 
-  if [ "$CONNS" -eq "0" ]; then
-    break
-  fi
-
-  sleep 1
-done
+#while true; do
+#  CONNS=$(netstat -ant | grep -w 127.0.0.1:2181 | grep -c ESTABLISHED)
+#
+#  if [ "$CONNS" -eq "0" ]; then
+#    break
+#  fi
+#
+#  sleep 1
+#done
 
 exit 0
--- topic-operator/scripts/topic_operator_lifecycle.sh
+++ topic-operator/scripts/topic_operator_lifecycle.sh
@@ -2,21 +2,26 @@
 set -e
 
 sleep 60
-a=1
-b=0
-while [ 200 -ne `curl -I -m 5 -s -w "%{http_code}\n" -o /dev/null  localhost:8080/ready` ]
-do
-  if [ $a -eq 10 ]
-  then
-    break
-  fi
-  sleep 2
-  b=1
-  a=$[$a+1]
-done
-if [ $b -eq 0 ]
-then
-  rm -f /etc/eto-certs/*.key
-  rm -f /etc/tls-sidecar/cluster-ca-certs/*.password
-  rm -f /etc/eto-certs/*.password
-fi
+#a=1
+#b=0
+#while [ 200 -ne `curl -I -m 5 -s -w "%{http_code}\n" -o /dev/null  localhost:8080/ready` ]
+#do
+#  if [ $a -eq 10 ]
+#  then
+#    break
+#  fi
+#  sleep 2
+#  b=1
+#  a=$[$a+1]
+#done
+#if [ $b -eq 0 ]
+#then
+#  rm -f /etc/eto-certs/*.key
+#  rm -f /etc/tls-sidecar/cluster-ca-certs/*.password
+#  rm -f /etc/eto-certs/*.password
+#fi
+
+sleep 60
+rm -f /etc/eto-certs/*.key
+rm -f /etc/tls-sidecar/cluster-ca-certs/*.password
+rm -f /etc/eto-certs/*.password
